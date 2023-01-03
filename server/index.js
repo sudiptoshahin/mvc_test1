851396560f0af919f4875687d63c3aadf6ed7b09
Sudipto Shahin <sudipto.shine@gmail.com>
2023-01-03 23:37:08 +0600

    dev testing server

--- server/index.js
+++ server/index.js
@@ -4,6 +4,12 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.json({ message: "Hello from server!" });
+  });
+
+
+
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on ${PORT} \n http:\\\\localhost:${PORT}`);
 });
